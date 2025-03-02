24917d53423aa5378166efdabeaf0a01254d136a
Phillip Jepsen <jepsens123@outlook.dk>
2025-03-02 16:00:28 +0100

    Change WeatherModel to have more suitable property names

--- Logic/Models/WeatherModel.cs
+++ Logic/Models/WeatherModel.cs
@@ -5,18 +5,43 @@ namespace WeatherApp.Logic.Models
 {
     public class WeatherModel
     {
-        public Coord coord { get; set; }
-        public List<Weather> weather { get; set; }
-        public Main main { get; set; }
-        public int visibility { get; set; }
-        public Wind wind { get; set; }
-        public Rain rain { get; set; }
-        public Clouds clouds { get; set; }
-        public int dt { get; set; }
-        public int timezone { get; set; }
-        public int id { get; set; }
-        public string name { get; set; }
+        [JsonProperty("coord")]
+        public Coord Coord { get; set; }
+
+        [JsonProperty("weather")]
+        public List<Weather> Weather { get; set; }
+
+        [JsonProperty("main")]
+        public Main Main { get; set; }
+
+        [JsonProperty("visibility")]
+        public int Visibility { get; set; }
+
+        [JsonProperty("wind")]
+        public Wind Wind { get; set; }
+
+        [JsonProperty("rain")]
+        public Rain Rain { get; set; }
+
+        [JsonProperty("snow")]
+        public Snow Snow { get; set; }
+
+        [JsonProperty("cloud")]
+        public Clouds Clouds { get; set; }
+
+        [JsonProperty("dt")]
+        public int DataCalculation { get; set; }
+
+        [JsonProperty("name")]
+        public string CityName { get; set; }
     }
+
+    public class Snow
+    {
+        [JsonProperty("1h")]
+        public double Precipitation { get; set; }
+    }
+
     // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
     public class Clouds
     {
@@ -28,39 +53,55 @@ namespace WeatherApp.Logic.Models
     {
         [JsonProperty("lon")]
         public double Longitude { get; set; }
+
         [JsonProperty("lat")]
         public double Latitude { get; set; }
     }
 
     public class Main
     {
-        public double temp { get; set; }
-        public double feels_like { get; set; }
-        public double temp_min { get; set; }
-        public double temp_max { get; set; }
-        public int pressure { get; set; }
-        public int humidity { get; set; }
-        public int sea_level { get; set; }
-        public int grnd_level { get; set; }
+        [JsonProperty("temp")]
+        public double Temperature { get; set; }
+
+        [JsonProperty("feels_like")]
+        public double Feelslike { get; set; }
+
+        [JsonProperty("temp_min")]
+        public double MinimumTemperature { get; set; }
+
+        [JsonProperty("temp_max")]
+        public double MaximumTemperature { get; set; }
+
+        [JsonProperty("humidity")]
+        public int Humidity { get; set; }
     }
 
     public class Rain
     {
         [JsonProperty("1h")]
-        public double _1h { get; set; }
+        public double Precipitation { get; set; }
     }
     public class Weather
     {
-        public int id { get; set; }
-        public string main { get; set; }
-        public string description { get; set; }
-        public string icon { get; set; }
+        [JsonProperty("id")]
+        public int WeatherConditionIdentifier { get; set; }
+
+        [JsonProperty("main")]
+        public string Main { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
     }
 
     public class Wind
     {
-        public double speed { get; set; }
-        public int deg { get; set; }
-        public double gust { get; set; }
+        [JsonProperty("speed")]
+        public double Speed { get; set; }
+
+        [JsonProperty("deg")]
+        public int DirectionDegrees { get; set; }
+
+        [JsonProperty("gust")]
+        public double Gust { get; set; }
     }
 }
